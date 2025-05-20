8f68e23d096f649b73ee71325e0d23f0aa0d0fc8
dzejkk <kontrajakub2@gmail.com>
2025-05-20 13:43:40 +0200

    added render UI func

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Constructor
+
 const myLibrary = [];
 
 function Book(title, year, pages, id) {
@@ -17,6 +19,27 @@ function addBookToLibrary(title, year, pages, id) {
 
 addBookToLibrary("Pan prstenov", 1998, 458, crypto.randomUUID());
 addBookToLibrary("Harry Potter", 2001, 469, crypto.randomUUID());
-addBookToLibrary("blue Hustler", 2011, 429, crypto.randomUUID());
+addBookToLibrary("Blue Hustler", 2011, 429, crypto.randomUUID());
 
 console.log(myLibrary);
+
+/// render UI
+
+function renderUI(data) {
+  const bookContainer = document.getElementById("book-container");
+
+  bookContainer.innerHTML = data
+    .map(
+      (book) =>
+        `<div class="book-item">
+        <h3>${book.title}</h3>
+        <p>Year: ${book.year}</p>
+        <p>Pages: ${book.pages}</p>
+      </div>`
+    )
+    .join("");
+}
+
+console.log(renderUI(myLibrary));
+
+renderUI(myLibrary);
