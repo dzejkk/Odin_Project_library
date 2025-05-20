9798bb93d384d16ee4ccd5c88c00f56cf044e619
dzejkk <kontrajakub2@gmail.com>
2025-05-20 13:09:26 +0200

    created constructor and store for new books

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("hello from app js");
+const myLibrary = [];
+
+function Book(title, year, pages, id) {
+  this.title = title;
+  this.year = year;
+  this.pages = pages;
+  this.id = id;
+}
+
+function addBookToLibrary(title, year, pages, id) {
+  const myFirstBook = new Book(title, year, pages, id);
+
+  myLibrary.push(myFirstBook);
+
+  return;
+}
+
+addBookToLibrary("Pan prstenov", 1998, 458, crypto.randomUUID());
+addBookToLibrary("Harry Potter", 2001, 469, crypto.randomUUID());
+addBookToLibrary("blue Hustler", 2011, 429, crypto.randomUUID());
+
+console.log(myLibrary);
